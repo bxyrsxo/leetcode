d2ab04218b46558e8a22aa68685318b8761407da
bxyrsxo <bxyrsxo@gmail.com>
2018-03-01 20:55:20 +0800

    Add files via upload

---
+++ 561. Array Partition I.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+	int arrayPairSum(vector<int>& nums) {
+		sort(nums.begin(), nums.end());
+		
+		int sum = 0;
+		for (int i = 0; i < nums.size(); i += 2)
+			sum += nums[i];
+		return sum;
+	}
+};
+
+int main()
+{
+	Solution sol;
+	
+	vector<int> vec = { 1,4,3,2 };
+	cout << sol.arrayPairSum(vec)<< endl;
+
+	system("pause");
+	return 0;
+}
+
+
+
+
+
+
+
